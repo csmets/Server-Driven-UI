2572f5f4dcc7bc7d518d9527e001bd0bdd903103
csmets <clyde.smets@gmail.com>
2022-05-26 21:23:37 +1000

    add remaining fragments

--- template-server/fragments.go
+++ template-server/fragments.go
@@ -6,7 +6,9 @@ type (
 	/*
 	   fragment feedContainerFragment on FeedContainer {
 	       elements {
-	           ...FeedHeadingFragment
+	           ...feedHeadingFragment
+	           ...typographyFragment
+	           ...feedItemFragment
 	       }
 	   }
 	*/
@@ -14,6 +16,7 @@ type (
 		Elements []struct {
 			FeedHeadingFragment       `graphql:"... on FeedHeading"`
 			TypographyContentFragment `graphql:"... on TypographyContent"`
+			FeedItemFragment          `graphql:"... on FeedItem"`
 		} `json:"elements,omitempty"`
 	}
 
@@ -57,7 +60,7 @@ type (
 	TypographyContentFragment struct {
 		Paragraph []struct {
 			ParagraphFragment `graphql:"... on Paragraph" json:",omitempty"`
-		} `json:",omitempty"`
+		} `json:"paragraph,omitempty"`
 	}
 
 	/*
@@ -67,5 +70,151 @@ type (
 	*/
 	ParagraphFragment struct {
 		Value graphql.String `json:"value"`
+	}
+
+	/*
+	   fragment feedItemFragment on FeedItem {
+	       items {
+	           ...columnLayoutFragment
+	           ...feedCaptionFragment
+	           ...feedImageFragment
+	       }
+	   }
+	*/
+	FeedItemFragment struct {
+		Items []struct {
+			Typename             graphql.String `graphql:"__typename"`
+			ColumnLayoutFragment `graphql:"... on ColumnLayout" json:",omitempty"`
+			FeedCaptionFragment  `graphql:"... on FeedCaption"`
+			FeedImageFragment    `graphql:"... on FeedImage"`
+		} `json:"items,omitempty"`
+	}
+
+	/*
+	   fragment columnLayoutFragment on ColumnLayout {
+	       columns {
+	           align
+	           ...feedFavouriteFragment
+	           ...feedFavouriteCount
+	       }
+	   }
+	*/
+	ColumnLayoutFragment struct {
+		Columns []struct {
+			Align                      graphql.String `json:"align,omitempty"`
+			FeedFavouriteFragment      `graphql:"... on FeedFavourite" json:",omitempty"`
+			FeedFavouriteCountFragment `graphql:"... on FeedFavouriteCount" json:",omitempty"`
+		} `json:"columns,omitempty"`
+	}
+
+	/*
+	   fragment feedfavouriteFragment on FeedFavourite {
+	       id
+	       icon
+	       signal {
+	           ...signalFragment
+	       }
+	       action {
+	           ...favouriteActionFragment
+	       }
+	   }
+	*/
+	FeedFavouriteFragment struct {
+		ID     graphql.String `graphql:"id" json:"id,omitempty"`
+		Icon   graphql.String `json:"icon,omitempty"`
+		Signal *struct {
+			SignalFragment `graphql:"... on Signal"`
+		} `json:"signal,omitempty"`
+		Action *struct {
+			FavouriteActionFragment `graphql:"... on FavouriteAction"`
+		} `json:"action,omitempty"`
+	}
+
+	/*
+	   fragment FavouriteActionFragment on FavouriteAction {
+	       feedId
+	       save {
+	           ...emitSignalFragment
+	       }
+	       unsave {
+	           ...emitSignalFragment
+	       }
+	   }
+	*/
+	FavouriteActionFragment struct {
+		FeedId graphql.String `json:"feedId,omitempty"`
+		Save   *struct {
+			EmitSignalFragment `graphql:"... on EmitSignal"`
+		} `json:"save,omitempty"`
+		Unsave *struct {
+			EmitSignalFragment `graphql:"... on EmitSignal"`
+		} `json:"unsave,omitempty"`
+	}
+
+	/*
+	   fragment emitSignalFragment on EmitSignal {
+	       signal {
+	           ...signalFragment
+	       }
+	       values {
+	           ...signalValuePairFragment
+	       }
+	   }
+	*/
+	EmitSignalFragment struct {
+		Signal *struct {
+			SignalFragment `graphql:"... on Signal"`
+		} `json:"signal,omitempty"`
+		Values *struct {
+			SignalValuePairFragment `graphql:"... on SignalValuePair"`
+		} `json:"values,omitempty"`
+	}
+
+	/*
+	   fragment signalValuePairFragment on SignalValuePair {
+	       key
+	       value
+	   }
+	*/
+	SignalValuePairFragment struct {
+		Key   graphql.String `json:"key,omitempty"`
+		Value graphql.String `json:"value,omitempty"`
+	}
+
+	/*
+	   fragment feedCaptionFragment on FeedCaption {
+	       text
+	   }
+	*/
+	FeedCaptionFragment struct {
+		Text graphql.String `json:"text,omitempty"`
+	}
+
+	/*
+	   fragment feedFavouriteCountFragment on FeedFavouriteCount {
+	       id
+	       count
+	       signal {
+	           ...signalFragment
+	       }
+	   }
+	*/
+	FeedFavouriteCountFragment struct {
+		ID     graphql.String `graphql:"id" json:"id,omitempty"`
+		Count  graphql.String `json:",omitempty"`
+		Signal *struct {
+			SignalFragment `graphql:"... on Signal"`
+		} `json:"signal,omitempty"`
+	}
+
+	/*
+	   fragment feedImageFragment on FeedImage {
+	       src
+	       alt
+	   }
+	*/
+	FeedImageFragment struct {
+		Src graphql.String `json:"src,omitempty"`
+		Alt graphql.String `json:"alt,omitempty"`
 	}
 )
--- template-server/go.mod
+++ template-server/go.mod
@@ -13,17 +13,19 @@ require (
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/golang/protobuf v1.3.5 // indirect
+	github.com/google/go-cmp v0.5.7 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/klauspost/compress v1.15.4 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/rogpeppe/go-internal v1.8.1 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
-	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
